2f6e14ccee7069df9a96ffc038e1be2302d9ed65
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2021-01-21 02:11:18 +0330

    auto convert mp4

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from argparse import ArgumentParser
-from sys import stdin, stdout
+from sys import stdin, stdout, stderr
 import os
 from PIL import Image, ImageSequence
 from PIL import ImageFont, ImageDraw
@@ -13,8 +13,15 @@ def open_file_to_read(parser, arg):
     if not os.path.exists(arg):
         parser.error(f"The file {arg} does not exist!")
         return None
+    elif not isinstance(arg, str):  # stdin default
+        return arg
+    elif ".gif" not in arg:  # convert with ffmpeg
+        print("inp is not gif, trying to convert", file=stderr)
+        converted_inp = f"{arg}.gif"
+        os.system(f"ffmpeg -i {arg} -y  {converted_inp}")
+        return open(converted_inp, 'rb')
     else:
-        return open(arg, 'rb') if isinstance(arg, str) else arg
+        return open(arg, 'rb')
 
 
 def open_file_to_write(parser, arg):
@@ -29,40 +36,44 @@ def save_gif(frames, out_file):
     frames[0].save(out_file,
                    save_all=True,
                    format="GIF",
-                   append_imgaes=frames[1:],
+                   append_imgaes=frames[1000:],
                    optimize=False)
+    out_file.close()
 
 
 i = 0
 
 
-def frame_write_gen(text):
+def frame_write_gen(text, font):
 
     place = (0, 0)
-    color = (255, 255, 255)
-    font_file = "./fonts/1.ttf"
-    font_size = 16
+    color = (255, 255, 2)
 
     def inner(frame):
         global i
-        print(f"frame #{i}")
         i += 1
 
         draw = ImageDraw.Draw(frame)
-        font = ImageFont.truetype(font_file, font_size)
         draw.text(place, text, color, font=font)
+        if i == 2:
+            frame.show()
+        #exit(0)
 
         return frame
     return inner
 
 
-def write_on_gif(inp_file, out_file, text):
+def write_on_gif(inp_file, out_file, text, font):
     inp_frames = load_gif_frames(inp_file)
-    print("load complete")
 
-    write_on_frame = frame_write_gen(text)
+    write_on_frame = frame_write_gen(text, font)
 
-    out_frames = [write_on_frame(frame) for frame in inp_frames]
+    #out_frames = [write_on_frame(frame) for frame in inp_frames]
+    out_frames = []
+    for f in inp_frames:
+        new_frame = write_on_frame(f)
+        out_frames.append(new_frame)
+    out_frames = out_frames[:2]
 
     save_gif(out_frames, out_file)
 
@@ -87,8 +98,11 @@ if __name__ == "__main__":
                         type=lambda x: open_file_to_write(parser, x),
                         default=stdout.buffer)
 
-    conf = parser.parse_args()
-    print(conf.input_file)
-    print(conf.output_file)
+    parser.add_argument("-f", "--font", dest="font",
+                        help="specify text font",
+                        type=lambda f: ImageFont.truetype(f, size=100),
+                        default=ImageFont.load_default())
 
-    write_on_gif(conf.input_file, conf.output_file, conf.text)
+    conf = parser.parse_args()
+
+    write_on_gif(conf.input_file, conf.output_file, conf.text, conf.font)
