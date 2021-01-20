6001724631805354ffb220794d1a6528a7e50859
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2021-01-20 23:34:42 +0330

    added argument parse

---
+++ main.py
@@ -0,0 +1,42 @@
+from argparse import ArgumentParser
+from sys import stdin, stdout
+import os
+from PIL import Image
+
+
+def open_file_to_read(parser, arg):
+    if not os.path.exists(arg):
+        parser.error(f"The file {arg} does not exist!")
+        return None
+    else:
+        return open(arg, 'r') if isinstance(arg, str) else arg
+
+
+def open_file_to_write(parser, arg):
+    return open(arg, "w") if isinstance(arg, str) else arg
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(
+        prog="main.py",
+        description="add text to gif image",
+        epilog="originally made for someone special :)"
+    )
+    parser.add_argument("-t", "--text", type=str, required=True,
+                        help="your text to be added on gif")
+    parser.add_argument("-i", "--input", dest="input_file",
+                        help="input gif as base file",
+                        metavar="FILE",
+                        type=lambda x: open_file_to_read(parser, x),
+                        default=stdin)
+
+    parser.add_argument("-o", "--output", dest="output_file",
+                        help="where to save result gif",
+                        metavar="FILE",
+                        type=lambda x: open_file_to_write(parser, x),
+                        default=stdout)
+
+    configs = parser.parse_args()
+    print(configs.input_file)
+    print(configs.output_file)
+
